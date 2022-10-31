0ddc3c6a111f7eb680ce2b3ab3dc113464137c90
Juan Marquez <juanmarquez4@gmail.com>
2022-10-31 17:23:00 +1100

    Updated assesment

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,10 +16,12 @@ function Project() {
                             Manage the stock of you business in a convenient way with the Stock Manager app.
 
                             </Card.Text>
-                            <Button variant="primary">Visit Project</Button>
+                            <Button variant="primary" href='https://stock-manager1234.herokuapp.com/'>Visit Project</Button>
+                            <Button className="my-3" variant="primary" href='https://github.com/juanmarquez4/stock-manager'>Visit Project's Repository</Button>
                         </Card.Body>
+                        
                     </Card>
-                    
+                    <Card.Subtitle className="m-3 text-muted">More projects coming soon {"-->"} </Card.Subtitle>
                 </Card.Body>
             </Card>
          
